3f480021c4ce6c74ef46b97812aee65b57de8fcd
barinova-alevel <oksana.barinova12@gmail.com>
2024-09-02 10:53:51 +0300

    The first unit test

--- Calculator.UnitTests/CalculatorTests.cs
+++ Calculator.UnitTests/CalculatorTests.cs
@@ -1,16 +1,24 @@
+using Calculator.BL;
+using Serilog;
+
 namespace Calculator.UnitTests
 {
+    [TestFixture]
     public class CalculatorTests
     {
-        [SetUp]
-        public void Setup()
+        [TestCase("5+2.5*2", 10)]
+        public void EvaluateExpression_InitTest(string expression, double expectedResult)
         {
-        }
+            // Arrange
+            CalculatorLogic cl = new CalculatorLogic();
+            double actualResult;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+            // Act
+            actualResult = cl.EvaluateExpression(expression);
+
+            // Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+            Console.WriteLine($"Actual result: {actualResult}, Expected result: {expectedResult}");
         }
     }
 }
