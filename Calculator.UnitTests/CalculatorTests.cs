71e1ca6ed8e63ac6266962bb49b0c7e60bd25f32
Oksana Barinova <novanasko@protonmail.com>
2024-09-03 12:16:53 +0300

    tests

--- Calculator.UnitTests/CalculatorTests.cs
+++ Calculator.UnitTests/CalculatorTests.cs
@@ -1,13 +1,19 @@
 using Calculator.BL;
-using Serilog;
 
 namespace Calculator.UnitTests
 {
     [TestFixture]
     public class CalculatorTests
     {
-        [TestCase("5+2.5*2", 10)]
-        public void EvaluateExpression_InitTest(string expression, double expectedResult)
+        [TestCase("5+2.5*2", 10, 
+            TestName = "EvaluateExpression_InitTest",
+            Description = "Check calculation of simple math expression.")]
+
+        [TestCase("-5+2.5*2", 0, 
+            TestName = "EvaluateExpression_ExpressionStartsFromNegativeNumber",
+            Description = "Check calculation of an expression when the first number is negative.")]
+
+        public void EvaluateExpression(string expression, double expectedResult)
         {
             // Arrange
             CalculatorLogic cl = new CalculatorLogic();
