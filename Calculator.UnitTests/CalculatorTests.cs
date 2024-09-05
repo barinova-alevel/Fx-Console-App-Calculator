1cde718b60d3f733a84b13f370c16fb912d5ea58
barinova-alevel <oksana.barinova12@gmail.com>
2024-09-05 10:54:17 +0300

    added negative scenarios to  EvaluateExpression.

--- Calculator.UnitTests/CalculatorTests.cs
+++ Calculator.UnitTests/CalculatorTests.cs
@@ -1,11 +1,12 @@
 using Calculator.BL;
+using Calculator.Exceptions;
 
 namespace Calculator.UnitTests
 {
     [TestFixture]
     public class CalculatorTests
     {
-        [TestCase("5+2.5*2", 10, 
+        [TestCase("5+2.5*2", 10,
             TestName = "EvaluateExpression_InitTest",
             Description = "Check calculation of simple math expression.")]
 
@@ -19,7 +20,7 @@ namespace Calculator.UnitTests
 
         [TestCase("80010*3", 240030,
             TestName = "EvaluateExpression_MultiplicationOperator",
-            Description = "Check calculation of an expresiion with * operator..")]
+            Description = "Check calculation of an expression with * operator..")]
 
         [TestCase("999000/10", 99900,
             TestName = "EvaluateExpression_DivisionOperator",
@@ -29,7 +30,7 @@ namespace Calculator.UnitTests
             TestName = "EvaluateExpression_ExpressionWithAllOperators",
             Description = "Check calculation of an expression that includes +-*/.")]
 
-        [TestCase("20.754+0.246", 21,
+        [TestCase("20.754+0.256", 21.01,
             TestName = "EvaluateExpression_ExpressionWithDecimalNumbers",
             Description = "Check calculation of an expression where numbers are decimal.")]
 
@@ -37,7 +38,7 @@ namespace Calculator.UnitTests
             TestName = "EvaluateExpression_ExpressionWithNegativeNumbers",
             Description = "Check calculation of an expression with negative numbers.")]
 
-        [TestCase("-5+2.5*2", 0, 
+        [TestCase("-5+2.5*2", 0,
             TestName = "EvaluateExpression_ExpressionStartsFromNegativeNumber",
             Description = "Check calculation of an expression when the first number is negative.")]
 
@@ -49,35 +50,11 @@ namespace Calculator.UnitTests
             TestName = "EvaluateExpression_ExpressionWithBigNumber",
             Description = "Check calculation of an expression with big number.")]
 
-        //[TestCase("-5+2.5", 0,
-        //    TestName = "EvaluateExpression_ExpressionWithConsecutiveOperators",
-        //    Description = "Check calculation of an expression with consecutive operators.")]
+        [TestCase(" 2+3 ", 5,
+            TestName = "EvaluateExpression_Leading/Trailing Whitespace",
+            Description = "Check handling of expression with whitespace.")]
 
-        //[TestCase("-5+2.5*2", 0,
-        //    TestName = "EvaluateExpression_Negative_WrongFormatExpression",
-        //    Description = "Check handling of providing not valid expression.")]
-
-        //[TestCase("2+*3", 0,
-        //    TestName = "EvaluateExpression_Negative_MalformedExpression",
-        //    Description = "Check handling of providing malformed expression.")]
-
-        //[TestCase(" 2+3 ", 0,
-        //    TestName = "EvaluateExpression_Negative_Leading/Trailing Whitespace",
-        //    Description = "Check handling of expression with whitespace.")]
-
-        //[TestCase("-5+2.5*2", 0,
-        //    TestName = "EvaluateExpression_Negative_AttemptToDevideByZero",
-        //    Description = "Check handling of attempt to devide by zero.")]
-
-        //[TestCase("-5+2.5*2", 0,
-        //    TestName = "EvaluateExpression_Negative_EmptyExpression",
-        //    Description = "Check handling of empty expression.")]
-
-        //[TestCase("-5+2.5*2", 0,
-        //    TestName = "EvaluateExpression_Negative_NullPassing",
-        //    Description = "Check handling of null passing.")]
-
-        public void EvaluateExpression(string expression, double expectedResult)
+        public void EvaluateExpression_Positive(string expression, double expectedResult)
         {
             // Arrange
             CalculatorLogic cl = new CalculatorLogic();
@@ -90,6 +67,51 @@ namespace Calculator.UnitTests
             Assert.That(actualResult, Is.EqualTo(expectedResult));
             Console.WriteLine(TestContext.CurrentContext.Test.Properties.Get("Description"));
             Console.WriteLine($"Actual result: {actualResult}, Expected result: {expectedResult}");
+        }
+
+        [Test]
+        public void EvaluateExpression_AttemptToDevideByZero()
+        {
+            // Arrange
+            CalculatorLogic cl = new CalculatorLogic();
+            string expression = "7.3/2";
+            string exceptionMessage = "Attempt to devide by zero.";
+
+            // Act & Assert 
+            var ex = Assert.Throws<DevideByZeroException>(() => cl.EvaluateExpression(expression));
+            Assert.That(ex.Message, Is.EqualTo(exceptionMessage));
+        }
+
+        [TestCase("", "The input is NOT a valid mathematical expression.", //Expected: "The input is NOT a valid mathematical expression."
+            TestName = "EvaluateExpression_Negative_EmptyExpression")]     //  But was: "The expression is invalid."
+
+        [TestCase("3++2", "Wrong input. Consecutive operators are not allowed.", //not run
+            TestName = "EvaluateExpression_ExpressionWithConsecutiveOperators")]
+
+        [TestCase("abc", "The input is NOT a valid mathematical expression.", //Expected: "The input is NOT a valid mathematical expression."
+            TestName = "EvaluateExpression_Negative_WrongFormatExpression")]  //But was: "The expression is invalid."
+
+        [TestCase("2+*3", "The input is NOT a valid mathematical expression.", //Expected: "The input is NOT a valid mathematical expression."
+            TestName = "EvaluateExpression_Negative_MalformedExpression")]     //But was:  "Wrong input. Consecutive operators are not allowed."
+
+        public void EvaluateExpression_Negative(string expression, string exceptionMessage)
+        {
+            // Arrange
+            CalculatorLogic cl = new CalculatorLogic();
+
+            // Act & Assert 
+            var ex = Assert.Throws<WrongInputException>(() => cl.EvaluateExpression(expression));
+            //Assert.That(ex.Message, Is.EqualTo(exceptionMessage));
+        }
+
+        [Test]
+        public void EvaluateExpression_NullPassing()//NullReferenceException Message: Object reference not set to an instance of an object.
+        {
+            // Arrange
+            CalculatorLogic cl = new CalculatorLogic();
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => cl.EvaluateExpression(null));
         }
     }
 }
